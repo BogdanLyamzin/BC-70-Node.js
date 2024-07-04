03d8d715ff8f6b91e7aaa98d1065c6749679380a
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2024-07-04 20:14:11 +0300

    finish lesson 7

--- lesson-6/project-backend/src/config.js
+++
@@ -1,1 +0,0 @@
-export const DB_HOST = "mongodb+srv://Bogdan:wqR9P9yNHt7Wu4OY@cluster0.yglxpd8.mongodb.net/my-songs?retryWrites=true&w=majority&appName=Cluster0";
--- lesson-6/project-backend/src/middlewares/notFoundHandler.js
+++
@@ -1,7 +0,0 @@
-const notFoundHandler = (req, res) => {
-    res.status(404).json({
-        message: `${req.url} Not Found`
-    })
-}
-
-export default notFoundHandler;
